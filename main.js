6c7a59eeda8bd64bdb0e52b93a6c7e3c2d392a8b
Christopher Ingram <cingram82@gmail.com>
2017-07-25 13:56:46 -0400

    all questions answered

--- main.js
+++ main.js
@@ -3,25 +3,25 @@
 // 1.
 // What does `givenName` equal right now?
 let givenName;
-// A:
+// A: undefined
 
 
 // 2.
 // What is `givenName` set to right now?
 givenName = "Tim";
-// A:
+// A: "Tim"
 
 
 // 3.
 // What is `givenName` set to right now?
 givenName = givenName;
-// A:
+// A: "Tim"
 
 
 // 4.
 // What is `greeting` set to?
 let greeting = "Hello, how are you? " + givenName;
-// A:
+// A: "Hello, how are you? Tim"
 
 
 // 5.
@@ -29,13 +29,13 @@ let greeting = "Hello, how are you? " + givenName;
 let high = 50;
 let low  = 10;
 let math = high - low;
-// A:
+// A: 40
 
 
 // 6.
 // What is `math` set to?
 math = high - "5";
-// A:
+// A: 50"5" wrong. returns 45. 505 would be using a + for concatenating. this is seeing that "5" has a number in the string and dose math with that.
 
 
 // 7.
@@ -44,20 +44,21 @@ math = high - "5";
 // The answer shoud not be written in a comment.
 let born = 1984;
 let today = 2017;
-// A:
+// A: let age = ("Tim is " + (today - born) + "years old")
 
 
 // 8.
 // Adjust this code. Store some information in the following variables.
 // A:
-let yourName;
-let instructorName;
+let yourName = "chris";
+let instructorName = "luke";
 
 
 // 9.
 // Update the variables `yourName` and `instructorName` so the expression below reads correctly.
 // A:
-
+yourName = "Chris Ingram";
+instructorName ="Ben";
 
 // This statement should read correctly
 let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
@@ -66,59 +67,59 @@ let statement = yourName + " is taking a class at The Iron Yard, my instructor's
 // 10.
 // We've created a variable named after each primitive type.
 // Use the `=` operator to assign a value to each variable matching the type in the name.
-let myNumber;
-let myString;
-let myBoolean;
+let myNumber = 42;
+let myString ="words";
+let myBoolean = true;
 // This one's a little tricky - think carefully about what makes a value "undefined"!
-let myUndefined;
-// A:
+let myUndefined ;
+// A: let myUndefined ;
 
 
 // For questions 11 - 19: List which boolean value will be stored in each variable. (Remember: `==` does not check data type)
 
 // 11.
 let x = (false == 0);
-// A.
+// A. true
 
 
 // 12.
 let y = (false == "");
-// A.
+// A. true
 
 
 // 13.
 let z = (0 == "");
-// A.
+// A.true
 
 
 // 14.
 let a = (null == null);
-// A.
+// A. true
 
 
 // 25.
 let b = (undefined == undefined);
-// A.
+// A. true
 
 
 // 16.
 let c = (undefined == null);
-// A.
+// A.true
 
 
 // 17.
 let d = (null == false);
-// A.
+// A. true
 
 
 // 18.
 let e = (NaN == null);
-// A.
+// A. true
 
 
 // 19.
 let f = (NaN == NaN);
-// A.
+// A. true
 
 
 // 20.
@@ -127,7 +128,7 @@ let f = (NaN == NaN);
 // A:
 let thirsty = true;
 
-{
+if (thirsty){
   console.log("I'm parched!");
 } else {
   console.log("I've had enough to drink.");
@@ -143,7 +144,7 @@ if(x){
 } else {
 	console.log("False!");
 }
-// A:
+// A: "false!"
 
 
 // 22.
@@ -152,7 +153,7 @@ if(""){
 } else {
 	console.log("False!");
 }
-// A:
+// A: false
 
 
 // 23.
@@ -164,7 +165,7 @@ if(x){
 } else {
 	console.log("False!");
 }
-// A:
+// A:false
 
 
 // 24.
@@ -173,7 +174,7 @@ if(false){
 } else {
 	console.log("False!");
 }
-// A:
+// A: false
 
 
 // 25.
@@ -182,7 +183,7 @@ if(-5 && 5){
 } else {
 	console.log("False!");
 }
-// A:
+// A: False gahh its true. (-5(truthy) && 5(truthy)) both are truthy.
 
 
 // 26.
@@ -191,7 +192,7 @@ if("Hello World!"){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 27.
@@ -200,7 +201,7 @@ if({}){
 } else {
 	console.log("False!");
 }
-// A:
+// A: false answer is true {} is creating an object so the if() sees an object inside it and if true.
 
 
 // 28.
@@ -209,7 +210,7 @@ if({name: "Yehuda Katz"}){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 29.
@@ -218,7 +219,7 @@ if(new Date()){
 } else {
 	console.log("False!");
 }
-// A:
+// A:  true
 
 
 // 30.
